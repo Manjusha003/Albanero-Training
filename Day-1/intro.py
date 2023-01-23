b8e790977dfc994620c1643826f9b4f6bd170a0b
Manjusha003 <manjusharaut003@gmail.com>
2023-01-23 20:42:53 +0530

    Day-1

---
+++ Day-1/intro.py
@@ -0,0 +1,54 @@
+
+# print("Hello World!!")
+
+# import sys
+# print("sys.argv[0] ==" ,sys.argv[0] )
+# print(sys.argv)
+
+
+# my_name=True
+# if my_name:
+#     print("Hello")
+
+
+# operators in python
+
+# a=30
+# b=20
+# c=a+b
+# print(c)
+# print(a*b)
+# print(a/b) # gives float value
+# print(a//b)  # gives int value
+# print(a%b) # remainder
+# print(a**b) # power 
+
+# print(4 * 3.75 - 1)
+
+
+
+# Strings can be concatenated (glued together) with the + operator, and repeated with *:
+
+# name="manjusha"
+# print(3*name) #manjushamanjushamanjusha
+# lastname="Raut"
+# print(name+lastname) #manjushaRaut
+
+# print('my' 'friends') #myfriends
+
+# indexing
+# word="python"
+# print(word[0]) #access character of string/varibale
+# print(word[-1]) #-ve indexs start counting from right
+
+# slicing
+# word="python"
+# print(word[1:2]) #fisrt index includeing last index excludeing
+# print(word[:3])
+# print(word[1:])
+
+
+#len()
+str="hello"
+print(len(str))
+
---
+++ Day-1/list.py
@@ -0,0 +1,11 @@
+# list=[7,9,2,8,1,0]
+# print(list[0])
+# print(list[-1])
+# print(list[:])
+# print(list[:2])
+
+
+# add/concat two lists
+list1=[6,8,2,90,13,44]
+list2=[7,3,1,3,91,9]
+print(list1+list2)
